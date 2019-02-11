8a08032ceccddf96e651839795bc79e701f02b1c
Adam L. Lane <alane019@gmail.com>
2019-02-11 01:00:33 -0500

    additional commit after initial commit

---
+++ src/main/java/com/tts/FizzBuzz/controllers/MainController.java
@@ -0,0 +1,40 @@
+package com.tts.FizzBuzz.controllers;
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+		@RequestMapping("/")
+		public String home(){
+			
+		ArrayList<String> solution = new ArrayList<String>();
+			
+			for(int i=1; i<=100; i++) {
+				
+				if(i % 3 == 0 && i % 5 == 0 ) {
+					solution.add("fizz buzz!");
+				}
+					
+			else if(i % 3 == 0) {
+				solution.add("fizz");
+			}
+			else if(i % 5 == 0) {
+				solution.add("buzz");
+			}
+			
+			else {
+			  solution.add(Integer.toString(i));
+			}
+					
+			}
+			
+			return("<h1>FizzBuzz</h1><div><p>" 
+					+ solution.toString()
+					+ "</p></div>");		
+		}
+
+	}
+
+
